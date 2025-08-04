c0c1e79effa98cc0c403a94c9ed379dfc60412aa
Evil Rabbit <evilrabbit.id@gmail.com>
2025-08-04 18:07:26 +0700

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,9 +4,8 @@ import { GeistMono } from 'geist/font/mono'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
+  title: 'Fluxly',
+  description: 'Fluxly keeps your processes moving effortlessly. Streamline workflows, gain insights, and empower your team with intelligent automation.',
 }
 
 export default function RootLayout({
